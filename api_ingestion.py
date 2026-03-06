1b83d0f15d4e886da063df046869a93dddfe7afe
guna-ui <gunasekhar449@live.com>
2026-03-06 18:21:55 +0530

    completed deduplication and bad data removal recieved from api

--- api_ingestion.py
+++ api_ingestion.py
@@ -1,6 +1,5 @@
 import requests
 
-
 def fetch_api(page):
     all_data=[]
     try:      
@@ -20,20 +19,58 @@ def fetch_api(page):
                 "html_url":results['html_url']              
            } 
             all_data.append(repo_data)
-        # page=page+1
         return all_data
 
     except:
         print(f"error occurred in the API fetching")
 
+# def check_duplicates(data):
+#      seen_repo_ids=set()
+#      clean_architecture=[]
+#      for check in data:
+#          seen_repo_ids.add(check['repo_id'])
+#      for repo in data:
+#          if repo['repo_id']  in seen_repo_ids:
+#              repo['create_at']=repo.get('create_at',{})[0:4]
+#              repo['language']=repo.get('language')or 'unknown'
+#              print(repo['language'])
+#              clean_architecture.append(repo)
+#      print(f"Total Records collected: {len(seen_repo_ids)} ,unique records: {len(clean_architecture)} processed,first 2 records:{clean_architecture[0:2]}",)
+
+#      return clean_architecture
+def check_duplicates(data):
+    seen_repo_ids = set()
+    clean_repositories = []
+
+    for repo in data:
+        repo_id = repo['repo_id']
+
+        if repo_id not in seen_repo_ids:   # check first
+            seen_repo_ids.add(repo_id)     # then mark as seen
+
+            # transformations
+            repo['create_at'] = repo.get('create_at', '')[0:4]
+            repo['language'] = repo.get('language') or 'unknown'
+
+            clean_repositories.append(repo)
+
+    print(f"Total Records collected: {len(data)}, unique records: {len(clean_repositories)}")
+    print("First 2 records:", clean_repositories[:2])
+
+    return clean_repositories
+   
+    
 
 
 if __name__=="__main__":
     page=1
-    total_count=0
+    all_repositories=[]
     while(page<=5):
-     data=fetch_api(page)
-     total_count=total_count+len(data)
-     print(f"Page{page} fetched {len(data)}")
-     page=page+1
-    print(f"Total repositories fetch:{total_count}")
+        data=fetch_api(page)
+        all_repositories.extend(data)
+        print(f"Page{page} fetched {len(data)}")
+        page=page+1
+    print(f"Total repositories fetch:{len(all_repositories)}")
+    new_data= check_duplicates(all_repositories)
+    print(new_data) 
+    
---
+++ sample.py
@@ -0,0 +1,10 @@
+data = {
+    "language": None,
+    "has_issues": True,
+    "has_projects": True,
+    "has_downloads": True,
+    "has_wiki": True,
+    "has_pages": False,
+}
+
+print(data.get('language','unknown'))
